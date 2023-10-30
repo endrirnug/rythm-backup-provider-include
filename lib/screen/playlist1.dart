33dd2e0888d52efc844a52ea80856e36598295f6
Ibnusabil43 <zaidan.697@gmail.com>
2023-10-30 14:35:25 +0700

    deleteSongbugfixed

--- lib/screen/playlist1.dart
+++ lib/screen/playlist1.dart
@@ -30,161 +30,165 @@ class _playlist1State extends State<playlist1> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1C1C27),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 37, vertical: 40),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(
-                  Icons.arrow_back_rounded,
-                  color: Color(0xFFD2AFFF),
-                  size: 33,
-                ),
-              ),
-              Text(
-                "Playlist",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xFFD2AFFF),
-                ),
-              ),
-              SizedBox(
-                width: 28,
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  ClipRRect(
-                    child: Image.file(
-                      File(widget.iniDaftarPlaylist.image),
-                      width: 250,
-                      height: 250,
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                  ),
-                ]),
-              ),
-              SizedBox(
-                height: 33,
-              ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 40, left: 25, right: 25, bottom: 0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.iniDaftarPlaylist.name,
-                        style: TextStyle(
-                          color: Color(0xFFD2AFFF),
-                          fontSize: 15,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w600,
-                          height: 0,
-                        ),
-                      ),
-                      Text(
-                        widget.iniDaftarPlaylist.desc,
-                        style: TextStyle(
-                          color: Color(0xFFD2AFFF),
-                          fontSize: 12,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w100,
-                          height: 0,
-                        ),
-                      ),
-                    ],
-                  ),
                   InkWell(
-                      onTap: () {
-                        if (widget.iniDaftarPlaylist.songList.length > 0) {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return Play(
-                                listSong: widget.iniDaftarPlaylist.songList,
-                                song: widget.iniDaftarPlaylist.songList[0],
-                                currIndex: 0);
-                          }));
-                        } else if (widget.iniDaftarPlaylist.songList.length ==
-                            0) {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return popUpWarning(
-                                  errorMessage: "Playlist ini kosong");
-                            },
-                          );
-                        }
-                      },
-                      child: Icon(Icons.play_circle_fill_rounded,
-                          color: Color(0xFFD2AFFF), size: 70)),
-                ],
-              )
-            ],
-          ),
-          InkWell(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return songListToAdd(playlist: widget.iniDaftarPlaylist);
-                }));
-              },
-              child: Container(
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                  Icon(Icons.add_box_outlined,
-                      color: Color(0xFFD2AFFF), size: 60),
-                  SizedBox(
-                    width: 10,
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back_rounded,
+                      color: Color(0xFFD2AFFF),
+                      size: 33,
+                    ),
                   ),
                   Text(
-                    "Add Song",
+                    "Playlist",
                     style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700,
                       color: Color(0xFFD2AFFF),
                     ),
+                  ),
+                  SizedBox(
+                    width: 28,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            child: Image.file(
+                              File(widget.iniDaftarPlaylist.image),
+                              width: 250,
+                              height: 250,
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                        ]),
+                  ),
+                  SizedBox(
+                    height: 33,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.iniDaftarPlaylist.name,
+                            style: TextStyle(
+                              color: Color(0xFFD2AFFF),
+                              fontSize: 15,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              height: 0,
+                            ),
+                          ),
+                          Text(
+                            widget.iniDaftarPlaylist.desc,
+                            style: TextStyle(
+                              color: Color(0xFFD2AFFF),
+                              fontSize: 12,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w100,
+                              height: 0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      InkWell(
+                          onTap: () {
+                            if (widget.iniDaftarPlaylist.songList.length > 0) {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return Play(
+                                    listSong: widget.iniDaftarPlaylist.songList,
+                                    song: widget.iniDaftarPlaylist.songList[0],
+                                    currIndex: 0);
+                              }));
+                            } else if (widget
+                                    .iniDaftarPlaylist.songList.length ==
+                                0) {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return popUpWarning(
+                                      errorMessage: "Playlist ini kosong");
+                                },
+                              );
+                            }
+                          },
+                          child: Icon(Icons.play_circle_fill_rounded,
+                              color: Color(0xFFD2AFFF), size: 70)),
+                    ],
                   )
-                ]),
-              )),
-          Expanded(
-            child: ListView.builder(
-              itemCount: context
-                  .watch<UsersProvider>()
-                  .playListArr[widget.currIdx]
-                  .songList
-                  .length,
-              itemBuilder: ((context, index) {
-                if (index <=
-                    context
-                        .watch<UsersProvider>()
-                        .playListArr[widget.currIdx]
-                        .songList
-                        .length) {
-                  return songListinPlaylist(
-                    iniDaftarPlaylist: widget.iniDaftarPlaylist,
-                    currIdx: index,
-                    listPlayList: context.watch<UsersProvider>(),
-                  );
-                }
-              }),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: context
+                      .watch<UsersProvider>()
+                      .playListArr[widget.currIdx]
+                      .songList
+                      .length,
+                  itemBuilder: ((context, index) {
+                    if (index <=
+                        context
+                            .watch<UsersProvider>()
+                            .playListArr[widget.currIdx]
+                            .songList
+                            .length) {
+                      return songListinPlaylist(
+                        iniDaftarPlaylist: widget.iniDaftarPlaylist,
+                        currIdx: index,
+                        listPlayList: context.watch<UsersProvider>(),
+                      );
+                    }
+                  }),
+                ),
+              ),
+            ]),
+          ),
+          Positioned(
+            right: 25,
+            bottom: 25,
+            child: FloatingActionButton(
+              backgroundColor: Color(0xFFD2AFFF),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        songListToAdd(playlist: widget.iniDaftarPlaylist),
+                  ),
+                );
+              },
+              child: Icon(
+                Icons.add_rounded,
+                size: 50,
+                color: Color(0xFF1C1C27),
+              ),
             ),
           ),
-        ]),
+        ],
       ),
     );
   }
@@ -312,8 +316,7 @@ class songListinPlaylist extends StatelessWidget {
                                   context
                                       .read<UsersProvider>()
                                       .deleteLagudariPlaylist(
-                                          playlist:
-                                              listPlayList.playListArr[currIdx],
+                                          playlist: iniDaftarPlaylist,
                                           song: iniDaftarPlaylist
                                               .songList[currIdx]);
                                   Navigator.pop(context);
